664fe45c7e37fd94bb7eefe0b91ef8aa18b03867
tanbbang01@gmail.com <tanbbang01@gmail.com>
2020-12-30 15:25:08 +0900

    20201230 Complete Section2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 // Import the React and ReactDOM libraries.
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react Component.
 const App = () => {
-  const buttonText = { text: 'click me' };
-  const labelText = 'Enter name : '
+  const buttonText = { text: "click me" };
+  const labelText = "Enter name : ";
   return (
     <div>
       <label className="label" htmlFor="name">
         {labelText}
       </label>
-      <input id="name" type="text"/>
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
         {buttonText.text}
       </button>
     </div>
@@ -21,4 +20,4 @@ const App = () => {
 };
 
 // Take the react component and show it on the screen.
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
